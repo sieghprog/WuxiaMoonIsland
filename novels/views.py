f1afb3800da1bf0645ccf32280c8a9231dca5fa7
sieghprog <caiosiegh.prog@gmail.com>
2022-08-04 23:14:50 -0300

    Contagem de capítulos e design parcial

--- novels/views.py
+++ novels/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Novel
+from django.db.models import Q, Count, Case, When
 
 class IndexView(ListView):
     model = Novel
     template_name = 'index.html'
+    vs = False
+    context_object_name = 'novels'
+    paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-novel_updated')
+        qs = qs.annotate(
+            chapters_count=Count(
+                Case(
+                    When(chapter__chapter_visible=True, then=1)
+                )
+            )
+        )
+        return qs
 
 
 class CategoryView(IndexView):
