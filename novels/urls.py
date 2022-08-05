a4a62b6ab51a3fd3073499e1b0c7049ab69b7a34
sieghprog <caiosiegh.prog@gmail.com>
2022-08-05 19:00:30 -0300

    Consulta de novels de x categoria

--- novels/urls.py
+++ novels/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index_view'),
-    path('category/<str:category>', views.CategoryView.as_view(), name='category_view'),
+    path('category/<str:category_name>', views.CategoryView.as_view(), name='category_view'),
     path('category_list/', views.CategoriesList.as_view(), name='category_list'),
     path('search/', views.IndexView.as_view(), name='search_view'),
     path('details/<int:pk>', views.DetailsView.as_view(), name='details_view'),
--- novels/views.py
+++ novels/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Novel
 from django.db.models import Q, Count, Case, When
+from categories.models import Category
 
 class IndexView(ListView):
     model = Novel
@@ -12,7 +13,7 @@ class IndexView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        qs = super().get_queryset()
+        qs = super().get_queryset().filter(novel_visible=True)
         qs = qs.order_by('-novel_updated')
         qs = qs.annotate(
             chapters_count=Count(
@@ -22,13 +23,35 @@ class IndexView(ListView):
             )
         )
         return qs
-
-
-class CategoryView(IndexView):
-    pass
-
 class CategoriesList(ListView):
-    pass
+    model = Category
+    template_name = 'categories_list.html'
+    context_object_name = 'categories'
+
+class CategoryView(CategoriesList):
+    model = Category
+    template_name = 'index.html'
+    context_object_name = 'novels'
+    def get_queryset(self):
+        qs = super().get_queryset()
+        print(self.kwargs.get('category_name', None))
+        category = (self.kwargs.get('category_name', None))
+
+        if not category:
+            return qs
+
+
+        qs = Novel.objects.filter(novel_categories__category_name=category, novel_visible=True)
+        qs = qs.annotate(
+            chapters_count=Count(
+                Case(
+                    When(chapter__chapter_visible=True, then=1)
+                )
+            )
+        )
+        print(Novel.objects.filter(novel_categories__category_name=category))
+        return qs
 
 class DetailsView(UpdateView):
-    pass
+    model = Novel
+    template_name = 'novel_details.html'
