b75688ca07bf3bb9ba1f9e974754931dd97af0f5
sieghprog <caiosiegh.prog@gmail.com>
2022-11-10 12:31:50 -0300

    Base da pagina de detalhes pronta. - Primeiras ideias para pagina de categoria

--- categories/models.py
+++ categories/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+    category_img = models.ImageField(upload_to='media/%Y/%m/%d', null=True, blank=True)
 
     def __str__(self):
         return self.category_name
